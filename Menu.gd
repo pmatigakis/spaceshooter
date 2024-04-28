2ea96d01c2fd6876c596ab5c7fa70f9af7f3aa61
Matigakis Panagiotis <pmatigakis@gmail.com>
2024-04-28 12:11:40 +0300

    Will use the same function to exit the game

--- Menu.gd
+++ Menu.gd
@@ -1,19 +1,9 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _input(event):
 	if event is InputEventKey and event.scancode == KEY_ESCAPE:
-		get_tree().quit()
+		_on_Exit_pressed()
 
 
 func _on_Start_pressed():
